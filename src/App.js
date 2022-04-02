690fe5748f232ad07729d4d5996abb67b3255307
XanderWitch <janell.robisch@gmail.com>
2022-04-01 20:06:25 -0400

    Figuring out how to route to subdirectory with react router for githu… …b pages

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default function App() {
 			<div className='mainBody'>
 				<Routes>
 					<BrowserRouter basename='/self-pub-axis'>
-						<Route path='/' element={<HomePage />} />
+						<Route path='/Home' element={<HomePage />} />
 						<Route
 							path='/Resources'
 							element={<Resources />}
