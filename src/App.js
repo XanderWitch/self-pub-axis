bf3da975ac66102cf8c566e3988955eaf22e96ec
XanderWitch <janell.robisch@gmail.com>
2022-04-02 20:44:42 -0400

    github pages mods

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ import Checklist from './Components/Checklist';
 import UpdateResource from './Components/Resources/UpdateResource';
 import Footer from './Components/Footer';
 import { Route, Routes } from 'react-router';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 export default function App() {
 	return (
 		<div>
 			<Navbar />
 			<div className='mainBody'>
-				<Router basename='/self-pub-axis'>
+				<BrowserRouter basename='/self-pub-axis'>
 					<Routes>
 						<Route path='/' element={<HomePage />} />
 						<Route
@@ -30,7 +30,7 @@ export default function App() {
 							element={<UpdateResource />}
 						/>
 					</Routes>
-				</Router>
+				</BrowserRouter>
 			</div>
 			<Footer />
 		</div>
